31c2873c785bccc13740796b69c6dd05165acc1c
Vienna Borowska <vienna.borowska@hotmail.com>
2022-01-22 18:42:56 +0000

    get requests for express routes

--- server.js
+++ server.js
@@ -5,15 +5,31 @@ const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+//Express variable
+const app = express();
 
 //File requires
 const savedNotes = require('./db/db.json');
-const { builtinModules } = require('module');
-
-//Global variables
-const app = express();
 
 //Express set-up
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+//Routing
+//GET requests
+app.get('/api/notes', (req, res) => {
+  res.json(savedNotes.slice(1));
+});
+// Set homepage path
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+//Set note html path
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+// Set random paths to homepage
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
